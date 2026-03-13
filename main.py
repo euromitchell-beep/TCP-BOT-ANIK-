963da797165c05acbb0c17309a604400ad7e0076
euromitchell-beep <euromitchell@gmail.com>
2026-03-13 22:22:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from google.protobuf.timestamp_pb2 import Timestamp
 from protobuf_decoder.protobuf_decoder import Parser
 
 # Local/Custom imports
-from pb2 import (
+from Pb2 import (
     DEcwHisPErMsG_pb2, MajoRLoGinrEs_pb2, PorTs_pb2, MajoRLoGinrEq_pb2, 
     sQ_pb2, Team_msg_pb2, RemoveFriend_Req_pb2, GetFriend_Res_pb2, 
     spam_request_pb2, devxt_count_pb2, dev_generator_pb2, kyro_title_pb2, room_join_pb2
